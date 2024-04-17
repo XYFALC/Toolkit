81150c3c0c5e71f7f92c3d41ecfe86226339becd
falco wenteler <falcowenteler@hotmail.com>
2024-04-17 12:38:19 +0200

    Add project files.

---
+++ Practice/InterfaceLinkedList.cs
@@ -0,0 +1,83 @@
+﻿// type parameter T in angle brackets
+public class GenericList<T>
+{
+    // The nested class is also generic on T.
+    private class Node
+    {
+        // T used in non-generic constructor.
+        public Node(T t)
+        {
+            next = null;
+            data = t;
+        }
+
+        private Node? next;
+        public Node? Next
+        {
+            get { return next; }
+            set { next = value; }
+        }
+
+        // T as private member data type.
+        private T data;
+
+        // T as return type of property.
+        public T Data
+        {
+            get { return data; }
+            set { data = value; }
+        }
+    }
+
+    private Node? head;
+
+    // constructor
+    public GenericList()
+    {
+        head = null;
+    }
+
+    // T as method parameter type:
+    public void AddHead(T t)
+    {
+        Node n = new Node(t);
+        n.Next = head;
+        head = n;
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        Node? current = head;
+
+        while (current != null)
+        {
+            yield return current.Data;
+            current = current.Next;
+        }
+    }
+
+
+}
+
+class TestGenericList
+{
+
+
+    static void Main()
+    {
+        // int is the type argument
+        GenericList<int> list = new GenericList<int>();
+
+        for (int x = 0; x < 1000; x++)
+        {
+            list.AddHead(x);
+
+        }
+
+        foreach (int i in list)
+        {
+            System.Console.Write(i + " ");
+        }
+        System.Console.WriteLine("\nDone");
+    }
+}
